b7f099dd28ccc917bb849a17eee63ea19ce2688b
U-T450S\Graham <graham@gellinghausen.com>
2018-12-01 22:35:04 -0600

    Whitespace cleanup

--- src/test/util/dollars_test.h
+++ src/test/util/dollars_test.h
@@ -53,50 +53,50 @@ private:
 	}
 	
 	void equals() {
-								  // c1<c2  c1=c2  c1>c2
+		                          // c1<c2  c1=c2  c1>c2
 		const bool expectations[] = {false, false, false,  // d1<d2
-									 false, true,  false,  // d1=d2
-									 false, false, false}; // d1>d2
+		                             false, true,  false,  // d1=d2
+		                             false, false, false}; // d1>d2
 		this->runComparisonExpectations(expectations, std::equal_to<Dollars>());
 	}
 	
 	void notEquals() {
-								  // c1<c2  c1=c2  c1>c2
+		                          // c1<c2  c1=c2  c1>c2
 		const bool expectations[] = {true,  true,  true,  // d1<d2
-									 true,  false, true,  // d1=d2
-									 true,  true,  true}; // d1>d2
+		                             true,  false, true,  // d1=d2
+		                             true,  true,  true}; // d1>d2
 		this->runComparisonExpectations(expectations, std::not_equal_to<Dollars>());
 	}
 	
 	void lessThan() {
-								  // c1<c2  c1=c2  c1>c2
+		                          // c1<c2  c1=c2  c1>c2
 		const bool expectations[] = {true,  true,  true,   // d1<d2
-									 true,  false, false,  // d1=d2
-									 false, false, false}; // d1>d2
+		                             true,  false, false,  // d1=d2
+		                             false, false, false}; // d1>d2
 		this->runComparisonExpectations(expectations, std::less<Dollars>());
 	}
 	
 	void lessThanOrEqual() {
-								  // c1<c2  c1=c2  c1>c2
+		                          // c1<c2  c1=c2  c1>c2
 		const bool expectations[] = {true,  true,  true,   // d1<d2
-									 true,  true,  false,  // d1=d2
-									 false, false, false}; // d1>d2
+		                             true,  true,  false,  // d1=d2
+		                             false, false, false}; // d1>d2
 		this->runComparisonExpectations(expectations, std::less_equal<Dollars>());
 	}
 	
 	void greaterThan() {
-								  // c1<c2  c1=c2  c1>c2
+		                          // c1<c2  c1=c2  c1>c2
 		const bool expectations[] = {false, false, false, // d1<d2
-									 false, false, true,  // d1=d2
-									 true,  true,  true}; // d1>d2
+		                             false, false, true,  // d1=d2
+		                             true,  true,  true}; // d1>d2
 		this->runComparisonExpectations(expectations, std::greater<Dollars>());
 	}
 	
 	void greaterThanOrEqual() {
-								  // c1<c2  c1=c2  c1>c2
+		                          // c1<c2  c1=c2  c1>c2
 		const bool expectations[] = {false, false, false, // d1<d2
-									 false, true,  true,  // d1=d2
-									 true,  true,  true}; // d1>d2
+		                             false, true,  true,  // d1=d2
+		                             true,  true,  true}; // d1>d2
 		this->runComparisonExpectations(expectations, std::greater_equal<Dollars>());
 	}
 	
