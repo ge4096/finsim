aa4a91a6b0b59454d7c9891688b31f3c663400be
U-T450S\Graham <graham@gellinghausen.com>
2018-12-08 02:35:30 -0600

    Build script improvements

--- build.sh
+++ build.sh
@@ -1,15 +1,79 @@
 #!/bin/bash
 
-COMPILER="g++ -std=c++11 -iquotesrc -g -Wall -Wextra"
-TARGET=finsim
+COMPILER="g++"
+COMPILER_FLAGS="-std=c++11 -iquotesrc -Wall -Wextra"
+DEBUG_FLAGS="-g -O0"
+OPTIMIZE_FLAGS="-O3"
+TARGET="finsim"
+TARGET_BASE_FOLDER="./bin/"
 
-if [ $# -eq 0 ] || [ "$1" = "debug" ]; then
-	${COMPILER} ./src/main.cpp -o ./bin/debug/${TARGET}
-elif [ "$1" = "test" ]; then
-	TEST_TARGET="./bin/debug/${TARGET}_test"
-	${COMPILER} ./src/test/main.cpp -o ${TEST_TARGET} && ${TEST_TARGET}
-elif [ "$1" = "release" ]; then
-	${COMPILER} -O3 ./src/main.cpp -o ./bin/release/${TARGET}
+if [ $# -eq 0 ]; then
+	BUILD_TYPE="debug"
 else
-	echo "Usage: ${0} [debug|test|release]"
+	BUILD_TYPE="$1"
 fi
+
+BUILD_MAIN=false
+BUILD_TEST=false
+RUN_TEST=false
+if [ "${BUILD_TYPE}" = "debug" ]; then
+	BUILD_MAIN=true
+	EXTRA_FLAGS="${DEBUG_FLAGS}"
+	TARGET_SUBFOLDER="debug/"
+elif [ "${BUILD_TYPE}" = "test" ]; then
+	BUILD_TEST=true
+	RUN_TEST=true
+	EXTRA_FLAGS="${DEBUG_FLAGS}"
+	TARGET_SUBFOLDER="debug/"
+elif [ "${BUILD_TYPE}" = "coverage" ]; then
+	BUILD_MAIN=true
+	BUILD_TEST=true
+	EXTRA_FLAGS="${DEBUG_FLAGS} -fprofile-arcs -ftest-coverage"
+	TARGET_SUBFOLDER="debug/"
+elif [ "${BUILD_TYPE}" = "profile" ]; then
+	BUILD_MAIN=true
+	BUILD_TEST=true
+	EXTRA_FLAGS="${OPTIMIZE_FLAGS} -pg"
+	TARGET_SUBFOLDER="debug/"
+elif [ "${BUILD_TYPE}" = "release" ]; then
+	BUILD_MAIN=true
+	EXTRA_FLAGS="${OPTIMIZE_FLAGS}"
+	TARGET_SUBFOLDER="release/"
+elif [ "${BUILD_TYPE}" = "clean" ]; then
+	rm -rf ${TARGET_BASE_FOLDER}
+	exit 0
+else
+	echo "Usage: ${0} [debug|test|coverage|profile|release|clean]"
+	exit 1
+fi
+
+START_TIME=$(date -u +%s)
+echo "Starting ${BUILD_TYPE} build"
+
+MAIN_TARGET="${TARGET_BASE_FOLDER}${TARGET_SUBFOLDER}${TARGET}"
+if [ "${BUILD_MAIN}" = true ]; then
+	mkdir -p $(dirname ${MAIN_TARGET})
+	${COMPILER} ${COMPILER_FLAGS} ${EXTRA_FLAGS} ./src/main.cpp -o ${MAIN_TARGET}
+	if [ $? -ne 0 ]; then
+		exit $?
+	fi
+fi
+
+TEST_TARGET="${TARGET_BASE_FOLDER}${TARGET_SUBFOLDER}${TARGET}_test"
+if [ "${BUILD_TEST}" = true ]; then
+	mkdir -p $(dirname ${TEST_TARGET})
+	${COMPILER} ${COMPILER_FLAGS} ${EXTRA_FLAGS} ./src/test/main.cpp -o ${TEST_TARGET}
+	if [ $? -ne 0 ]; then
+		exit $?
+	fi
+fi
+
+if [ "${RUN_TEST}" = true ]; then
+	${TEST_TARGET}
+	if [ $? -ne 0 ]; then
+		exit $?
+	fi
+fi
+
+END_TIME=$(date -u +%s)
+echo "Build completed successfully in $((${END_TIME}-${START_TIME})) seconds"
